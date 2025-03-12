f009a1d3ceb449a610f21452274ceaea4f63d0b8
theprogmatheus <theprog.matheus@gmail.com>
2025-03-12 10:09:37 -0300

    Setup CommandSet

--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandSet.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandSet.java
@@ -1,52 +1,45 @@
 package com.github.theprogmatheus.mc.solaryeconomy.command;
 
 import com.github.theprogmatheus.mc.solaryeconomy.SolaryEconomy;
-import com.github.theprogmatheus.mc.solaryeconomy.config.Lang;
 import com.github.theprogmatheus.mc.solaryeconomy.database.entity.BankAccountEntity;
 import com.github.theprogmatheus.mc.solaryeconomy.service.EconomyService;
+import com.github.theprogmatheus.mc.solaryeconomy.util.InputParse;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import java.math.BigDecimal;
-import java.text.MessageFormat;
 
 public class CommandSet extends AbstractCommandExecutor {
+
+    private final EconomyService economyService;
 
     public CommandSet() {
         super(new String[]{"set"}, "set", "set <player> <newBalance>");
         this.economyService = SolaryEconomy.getInstance().getEconomyService();
     }
 
-    private final EconomyService economyService;
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
         if (args.length == 2) {
-            String accountId = args[0].toLowerCase();
-
-
-            BankAccountEntity account = this.economyService.getDefaultAccount(accountId);
+            BankAccountEntity account = this.economyService.getDefaultAccount(InputParse.parseAccountId(args[0]));
             if (account == null) {
-                sender.sendMessage(MessageFormat.format(Lang.ACCOUNT_NOT_FOUND, accountId));
-                return false;
+                sender.sendMessage("§aA conta que você informou, não existe.");
+                return true;
             }
 
-            BigDecimal value = null;
-            try {
-                value = new BigDecimal(args[1]);
-                if (value.doubleValue() < 0)
-                    throw new RuntimeException(MessageFormat.format(Lang.VALUE_CANT_BE_NEGATIVE, value));
-            } catch (Exception e) {
-                sender.sendMessage(MessageFormat.format(Lang.INVALID_VALUE, args[1], e.getMessage()));
-                return false;
+            BigDecimal value = InputParse.parseBigDecimal(args[1]);
+            if (value == null || value.doubleValue() < 0) {
+                sender.sendMessage("§cValor inválido.");
+                return true;
             }
+
             account.setBalance(value);
+            account = this.economyService.saveInCache(account);
 
-            // save account
-            this.economyService.getAccountCache().put(account);
-
-            sender.sendMessage(MessageFormat.format(Lang.BALANCE_SET_SUCCESS, account.getOwnerName(), account.getBalance().toPlainString()));
+            sender.sendMessage("§aVocê definiu o saldo de " + account.getOwnerName() + " para " + value.toPlainString());
         }
+
         return false;
     }
 }
