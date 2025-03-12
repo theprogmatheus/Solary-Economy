1c664ce169791c4877a93d5251d47c038852a489
theprogmatheus <theprog.matheus@gmail.com>
2025-03-12 10:15:20 -0300

    Setup SecoCommand

--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandSeco.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandSeco.java
@@ -1,51 +1,24 @@
 package com.github.theprogmatheus.mc.solaryeconomy.command;
 
 import com.github.theprogmatheus.mc.solaryeconomy.SolaryEconomy;
-import com.github.theprogmatheus.mc.solaryeconomy.database.entity.BankAccountEntity;
-import com.github.theprogmatheus.mc.solaryeconomy.service.EconomyService;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.Arrays;
 
-public class CommandSeco implements CommandExecutor {
-
-    private AbstractCommandExecutor[] subCommands;
+public class CommandSeco extends AbstractCommandExecutor {
 
     public CommandSeco() {
-        this.registerSubCommands(new CommandSet(), new CommandAdd(), new CommandRemove());
-        this.economyService = SolaryEconomy.getInstance().getEconomyService();
+        super(new String[]{"seco", "solaryeconomy"}, "solaryeconomy", "seco");
     }
-
-    private final EconomyService economyService;
-
-
-    public void registerSubCommands(AbstractCommandExecutor... subCommands) {
-        this.subCommands = subCommands;
-    }
-
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        if (args.length == 0) {
-            if (!(sender instanceof Player)) return false;
-
-            BankAccountEntity account = this.economyService.getDefaultAccountOrCreateIfNotExists((Player) sender);
-
-            sender.sendMessage("§aSaldo: " + account.getBalance().toPlainString());
-            return false;
-        }
-
-        for (AbstractCommandExecutor subCommand : this.subCommands) {
-            for (String alias : subCommand.getCommands()) {
-                if (!args[0].equalsIgnoreCase(alias)) continue;
-                return subCommand.onCommand(sender, command, label, Arrays.copyOfRange(args, 1, args.length));
-            }
-        }
-
+        SolaryEconomy plugin = SolaryEconomy.getInstance();
+        sender.sendMessage("§eRunning " + plugin.getName() + " on " + plugin.getDescription().getVersion() + " version");
+        sender.sendMessage("§eAuthors: " + Arrays.toString(plugin.getDescription().getAuthors().toArray()));
         return false;
     }
+
+
 }
