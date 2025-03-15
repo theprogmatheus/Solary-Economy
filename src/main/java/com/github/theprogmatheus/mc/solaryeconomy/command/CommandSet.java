c1773d8b7f9c05d30a7fd5baa47001fcecda13c1
theprogmatheus <theprog.matheus@gmail.com>
2025-03-15 09:11:40 -0300

    Setup CommandSet messages

--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandSet.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandSet.java
@@ -1,6 +1,7 @@
 package com.github.theprogmatheus.mc.solaryeconomy.command;
 
 import com.github.theprogmatheus.mc.solaryeconomy.SolaryEconomy;
+import com.github.theprogmatheus.mc.solaryeconomy.config.Lang;
 import com.github.theprogmatheus.mc.solaryeconomy.database.entity.BankAccountEntity;
 import com.github.theprogmatheus.mc.solaryeconomy.service.EconomyService;
 import com.github.theprogmatheus.mc.solaryeconomy.util.InputParse;
@@ -8,6 +9,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 import java.math.BigDecimal;
+import java.text.MessageFormat;
 
 public class CommandSet extends AbstractCommand {
 
@@ -24,20 +26,24 @@ public class CommandSet extends AbstractCommand {
         if (args.length == 2) {
             BankAccountEntity account = this.economyService.getDefaultAccount(InputParse.parseAccountId(args[0]));
             if (account == null) {
-                sender.sendMessage("§aA conta que você informou, não existe.");
+                sender.sendMessage(MessageFormat.format(Lang.ACCOUNT_NOT_FOUND, args[0]));
                 return true;
             }
 
             BigDecimal value = InputParse.parseBigDecimal(args[1]);
-            if (value == null || value.doubleValue() < 0) {
-                sender.sendMessage("§cValor inválido.");
+            if (value == null) {
+                sender.sendMessage(MessageFormat.format(Lang.INVALID_INPUT_VALUE, args[1]));
+                return true;
+            }
+            if (value.doubleValue() <= 0) {
+                sender.sendMessage(MessageFormat.format(Lang.VALUE_CANT_BE_NEGATIVE, value.toPlainString()));
                 return true;
             }
 
             account.setBalance(value);
             account = this.economyService.saveInCache(account);
 
-            sender.sendMessage("§aVocê definiu o saldo de " + account.getOwnerName() + " para " + value.toPlainString());
+            sender.sendMessage(MessageFormat.format(Lang.BALANCE_SET_SUCCESS, value.toPlainString(), account.getOwnerName()));
         }
 
         return false;
