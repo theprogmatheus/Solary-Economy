cb8c48fe01733f0821f248cc4c84299dbdefb537
theprogmatheus <theprog.matheus@gmail.com>
2025-03-15 09:39:16 -0300

    Add color in update message, sync update message

--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/service/UpdaterService.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/service/UpdaterService.java
@@ -1,13 +1,16 @@
 package com.github.theprogmatheus.mc.solaryeconomy.service;
 
+import com.github.theprogmatheus.mc.solaryeconomy.SolaryEconomy;
 import com.github.theprogmatheus.util.JGRUChecker;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
 @Getter
 public class UpdaterService implements Service {
+
 
     private final JavaPlugin plugin;
     private JGRUChecker updateChecker;
@@ -22,6 +25,8 @@ public class UpdaterService implements Service {
 
         // check for new updates
         this.checkNewUpdates();
+
+
     }
 
     @Override
@@ -36,21 +41,33 @@ public class UpdaterService implements Service {
 
         this.updateChecker.checkAsync().thenAcceptAsync(release -> {
             if (release == null || this.updateChecker.getCurrentVersion().equals(release.getVersion())) return;
-            Logger log = this.plugin.getLogger();
+            Bukkit.getScheduler().runTask(SolaryEconomy.getInstance(), () -> {
 
-            System.out.println(" ");
-            log.info("===============================================");
-            log.info("[!] A new update is available! [!]");
-            log.info("===============================================");
-            log.info(" * Plugin: " + release.getName());
-            log.info(" * Current Version: " + this.updateChecker.getCurrentVersion());
-            log.info(" * New Version: " + release.getVersion());
-            log.info(" * Download here: " + release.getDownloadPage());
-            log.info("===============================================");
-            log.info("[!] Upgrade now to enjoy the latest features, improvements, and bug fixes!");
-            log.info("[!] Staying updated ensures better performance and security.");
-            log.info("===============================================");
-            System.out.println(" ");
+                Logger log = this.plugin.getLogger();
+
+                // ANSI escape code
+                String ANSI_RESET = "\u001B[0m";
+                String ANSI_YELLOW = "\u001B[93m";
+                String ANSI_CYAN = "\u001B[36m";
+                String ANSI_WHITE = "\u001B[37m";
+                String ANSI_GOLD = "\u001B[33m";
+
+
+                System.out.println(" ");
+                log.info(ANSI_YELLOW + "===============================================" + ANSI_RESET);
+                log.info(ANSI_GOLD + "[!] A new update is available! [!]" + ANSI_RESET);
+                log.info(ANSI_YELLOW + "===============================================" + ANSI_RESET);
+                log.info(ANSI_CYAN + " * Plugin: " + ANSI_WHITE + release.getName() + ANSI_RESET);
+                log.info(ANSI_CYAN + " * Current Version: " + ANSI_WHITE + this.updateChecker.getCurrentVersion() + ANSI_RESET);
+                log.info(ANSI_CYAN + " * New Version: " + ANSI_WHITE + release.getVersion() + ANSI_RESET);
+                log.info(ANSI_CYAN + " * Download here: " + ANSI_WHITE + release.getDownloadPage() + ANSI_RESET);
+                log.info(ANSI_YELLOW + "===============================================" + ANSI_RESET);
+                log.info(ANSI_GOLD + "[!] Upgrade now to enjoy the latest features, improvements, and bug fixes!" + ANSI_RESET);
+                log.info(ANSI_GOLD + "[!] Staying updated ensures better performance and security." + ANSI_RESET);
+                log.info(ANSI_YELLOW + "===============================================" + ANSI_RESET);
+                System.out.println(" ");
+            });
+
         });
     }
 }
