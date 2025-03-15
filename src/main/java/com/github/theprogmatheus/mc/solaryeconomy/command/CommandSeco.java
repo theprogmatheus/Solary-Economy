4ac0f3118fbf10718c728928ae5cf1291c8035e1
theprogmatheus <theprog.matheus@gmail.com>
2025-03-15 09:09:25 -0300

    Update CommandSeco

--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandSeco.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandSeco.java
@@ -9,14 +9,16 @@ import java.util.Arrays;
 public class CommandSeco extends AbstractCommand {
 
     public CommandSeco() {
-        super(new String[]{"seco", "solaryeconomy"}, "Default plugin command", "solaryeconomy", "seco");
+        super(new String[]{"seco", "solaryeconomy"}, "Default plugin command", null, "seco");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         SolaryEconomy plugin = SolaryEconomy.getInstance();
-        sender.sendMessage("§eRunning " + plugin.getName() + " on " + plugin.getDescription().getVersion() + " version");
-        sender.sendMessage("§eAuthors: " + Arrays.toString(plugin.getDescription().getAuthors().toArray()));
+        sender.sendMessage(" ");
+        sender.sendMessage("§6Running §f" + plugin.getName() + "§6 on §f" + plugin.getDescription().getVersion() + "§6 version");
+        sender.sendMessage("§6Authors: §f" + Arrays.toString(plugin.getDescription().getAuthors().toArray()));
+        sender.sendMessage(" ");
         return false;
     }
 
