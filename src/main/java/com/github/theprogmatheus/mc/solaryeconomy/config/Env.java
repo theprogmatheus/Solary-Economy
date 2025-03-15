52286b0bb5955779aff3314f97b51bdce2a6ed50
theprogmatheus <theprog.matheus@gmail.com>
2025-03-15 12:32:51 -0300

    Create config Env

---
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/config/Env.java
@@ -0,0 +1,26 @@
+package com.github.theprogmatheus.mc.solaryeconomy.config;
+
+public class Env {
+
+    /*
+     * Database config
+     */
+    public static String DATABASE_TYE;
+    public static String DATABASE_ADDRESS;
+    public static String DATABASE_NAME;
+    public static String DATABASE_USERNAME;
+    public static String DATABASE_PASSWORD;
+
+
+    /*
+     * Cache config
+     */
+    public static int CACHE_ACCOUNTS_MAX_SIZE;
+    public static long CACHE_ACCOUNTS_EXPIRE;
+    public static long CACHE_ACCOUNTS_AUTO_FLUSH_DELAY;
+
+    /*
+     * Lang config
+     */
+    public static String LANG;
+}
