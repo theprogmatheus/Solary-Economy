ba5a6f61ffaa190674fdce062eb443e6dbe9bd96
theprogmatheus <theprog.matheus@gmail.com>
2025-03-15 01:14:25 -0300

    command register in runtime

--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/AbstractCommandExecutor.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/AbstractCommandExecutor.java
@@ -1,25 +1,63 @@
 package com.github.theprogmatheus.mc.solaryeconomy.command;
 
-import lombok.AllArgsConstructor;
+import com.github.theprogmatheus.mc.solaryeconomy.SolaryEconomy;
 import lombok.Getter;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
+import org.bukkit.Bukkit;
+import org.bukkit.command.*;
+import org.bukkit.command.defaults.BukkitCommand;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 
 @Getter
-@AllArgsConstructor
-public abstract class AbstractCommandExecutor implements CommandExecutor, TabCompleter {
+public abstract class AbstractCommandExecutor extends BukkitCommand implements CommandExecutor, TabCompleter {
+
 
     private String[] commands;
     private String permission;
     private String usage;
 
+    protected AbstractCommandExecutor(String[] commands, String permission, String usage) {
+        super(commands[0]);
+
+        this.commands = commands;
+        super.setUsage(this.usage = usage);
+        super.setPermission(this.permission = permission);
+        super.setPermissionMessage("§cVocê não tem permissão para isso.");
+        if (commands.length > 1)
+            super.setAliases(new ArrayList<>(Arrays.asList(Arrays.copyOfRange(commands, 1, commands.length))));
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        return onCommand(sender, this, commandLabel, args);
+    }
+
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         return List.of();
+    }
+
+    public static <C extends AbstractCommandExecutor> C register(C command) {
+        if (command == null) return null;
+        CommandMap commandMap;
+        try {
+            Field field = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            field.setAccessible(true);
+            commandMap = (CommandMap) field.get(Bukkit.getServer());
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+
+        Logger log = SolaryEconomy.getInstance().getLogger();
+
+        log.info("Register command: " + command.getLabel() + " with usage: " + command.getUsage() + " with description: " + command.getDescription() + " with aliases: " + command.getAliases());
+
+        commandMap.register("solaryeconomy", command);
+        return command;
     }
 }
--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/service/CommandService.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/service/CommandService.java
@@ -2,12 +2,7 @@ package com.github.theprogmatheus.mc.solaryeconomy.service;
 
 import com.github.theprogmatheus.mc.solaryeconomy.command.*;
 import lombok.Getter;
-import org.bukkit.Bukkit;
-import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 @Getter
 public class CommandService implements Service {
@@ -27,33 +22,12 @@ public class CommandService implements Service {
 
     @Override
     public void startup() {
-        register(this.commandSeco = new CommandSeco());
-        register(this.commandBalance = new CommandBalance());
-        register(this.commandSet = new CommandSet());
-        register(this.commandAdd = new CommandAdd());
-        register(this.commandRemove = new CommandRemove());
+        this.commandSeco = AbstractCommandExecutor.register(new CommandSeco());
+        this.commandBalance = AbstractCommandExecutor.register(new CommandBalance());
+        this.commandSet = AbstractCommandExecutor.register(new CommandSet());
+        this.commandAdd = AbstractCommandExecutor.register(new CommandAdd());
+        this.commandRemove = AbstractCommandExecutor.register(new CommandRemove());
     }
-
-
-    public void register(AbstractCommandExecutor executor) {
-        if (executor == null) return;
-
-        String[] commands = executor.getCommands();
-        PluginCommand command = this.plugin.getCommand(commands[0]);
-        if (command == null) return;
-
-        command.setPermission(executor.getPermission());
-        command.setPermissionMessage("§cVocê não tem permissão para usar isso.");
-        command.setUsage(executor.getUsage());
-
-        // aliasses does not working
-        if (commands.length > 1)
-            command.setAliases(new ArrayList<>(Arrays.asList(Arrays.copyOfRange(commands, 1, commands.length))));
-        command.setExecutor(executor);
-        command.setTabCompleter(executor);
-
-    }
-
 
     @Override
     public void shutdown() {
