bb6bf1e5843fe4019930994fbd1e333089473da0
theprogmatheus <theprog.matheus@gmail.com>
2025-03-16 18:30:12 -0300

    add currency formatter support on config

--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/config/Env.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/config/Env.java
@@ -30,6 +30,8 @@ public class Env {
      */
     public static String ECONOMY_CURRENCY_SINGULAR;
     public static String ECONOMY_CURRENCY_PLURAL;
+    public static int ECONOMY_CURRENCY_FORMATTER_PRECISION;
+    public static boolean ECONOMY_CURRENCY_FORMATTER_WITH_TAG;
     public static double ECONOMY_START_VALUE;
     public static int ECONOMY_RANK_SIZE;
     public static long ECONOMY_RANK_UPDATE_DELAY;
