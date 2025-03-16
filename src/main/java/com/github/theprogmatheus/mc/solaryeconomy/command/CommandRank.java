995c9939ce24af4815cabdec6d80d12e90911136
theprogmatheus <theprog.matheus@gmail.com>
2025-03-16 15:08:34 -0300

    update rank command to use custom messages

--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandRank.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandRank.java
@@ -1,10 +1,12 @@
 package com.github.theprogmatheus.mc.solaryeconomy.command;
 
 import com.github.theprogmatheus.mc.solaryeconomy.SolaryEconomy;
+import com.github.theprogmatheus.mc.solaryeconomy.config.Lang;
 import com.github.theprogmatheus.mc.solaryeconomy.database.entity.BankAccountEntity;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 public class CommandRank extends AbstractCommand {
@@ -16,12 +18,13 @@ public class CommandRank extends AbstractCommand {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         List<BankAccountEntity> rank = SolaryEconomy.getInstance().getEconomyService().getAccountCache().getAccountsRank();
-        sender.sendMessage("§6Rank de jogadores mais ricos:");
+
+        sender.sendMessage(MessageFormat.format(Lang.COMMAND_RANK_FORMAT_HEADER, rank.size()));
         for (int i = 0; i < rank.size(); i++) {
             BankAccountEntity account = rank.get(i);
-            sender.sendMessage("§a(" + (i + 1) + ") " + account.getOwnerName() + ": " + account.getBalance().toPlainString());
-
+            sender.sendMessage(MessageFormat.format(Lang.COMMAND_RANK_FORMAT_BODY, i, account.getOwnerName(), account.getBalance().toPlainString()));
         }
+        sender.sendMessage(MessageFormat.format(Lang.COMMAND_RANK_FORMAT_FOOTER, rank.size()));
         return true;
     }
 }
