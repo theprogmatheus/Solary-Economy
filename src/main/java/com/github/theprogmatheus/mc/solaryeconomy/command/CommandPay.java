5e88218e75a06df6e1d0505d13a21329cedca133
theprogmatheus <theprog.matheus@gmail.com>
2025-03-16 22:55:09 -0300

    Fix command pay running on console

--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandPay.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/command/CommandPay.java
@@ -24,44 +24,44 @@ public class CommandPay extends AbstractCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (checkPlayer(sender) == null) return true;
+
         if (args.length == 2) {
-            if (sender instanceof Player) {
-                BankAccountEntity account = this.economyService.getDefaultAccountOrCreateIfNotExists((Player) sender);
-                BankAccountEntity targetAccount = this.economyService.getDefaultAccount(InputParse.parseAccountId(args[0]));
+            BankAccountEntity account = this.economyService.getDefaultAccountOrCreateIfNotExists((Player) sender);
+            BankAccountEntity targetAccount = this.economyService.getDefaultAccount(InputParse.parseAccountId(args[0]));
 
-                if (targetAccount == null) {
-                    sender.sendMessage(MessageFormat.format(Lang.ACCOUNT_NOT_FOUND, args[0]));
-                    return true;
-                }
-
-                BigDecimal value = InputParse.parseBigDecimal(args[1]);
-                if (value == null) {
-                    sender.sendMessage(MessageFormat.format(Lang.INVALID_INPUT_VALUE, args[1]));
-                    return true;
-                }
-                if (value.doubleValue() <= 0) {
-                    sender.sendMessage(MessageFormat.format(Lang.VALUE_CANT_BE_NEGATIVE, this.economyService.formatBigDecimal(value)));
-                    return true;
-                }
-
-                if (account.getBalance().doubleValue() < value.doubleValue()) {
-                    sender.sendMessage(MessageFormat.format(Lang.BALANCE_HAS_NO_FUNDS, this.economyService.formatBigDecimal(account.getBalance()), this.economyService.formatBigDecimal(value)));
-                    return true;
-                }
-
-                account.setBalance(account.getBalance().subtract(value));
-                targetAccount.setBalance(targetAccount.getBalance().add(value));
-
-                account = this.economyService.saveInCache(account);
-                targetAccount = this.economyService.saveInCache(targetAccount);
-
-                sender.sendMessage(MessageFormat.format(Lang.PAYMENT_SEND_SUCCESS, this.economyService.formatCurrency(value), targetAccount.getOwnerName()));
-
-                Player targetPlayer = Bukkit.getPlayer(targetAccount.getOwnerName());
-                if (targetPlayer != null)
-                    targetPlayer.sendMessage(MessageFormat.format(Lang.PAYMENT_RECEIVED_SUCCESS, this.economyService.formatCurrency(value), account.getOwnerName()));
-
+            if (targetAccount == null) {
+                sender.sendMessage(MessageFormat.format(Lang.ACCOUNT_NOT_FOUND, args[0]));
+                return true;
             }
+
+            BigDecimal value = InputParse.parseBigDecimal(args[1]);
+            if (value == null) {
+                sender.sendMessage(MessageFormat.format(Lang.INVALID_INPUT_VALUE, args[1]));
+                return true;
+            }
+            if (value.doubleValue() <= 0) {
+                sender.sendMessage(MessageFormat.format(Lang.VALUE_CANT_BE_NEGATIVE, this.economyService.formatBigDecimal(value)));
+                return true;
+            }
+
+            if (account.getBalance().doubleValue() < value.doubleValue()) {
+                sender.sendMessage(MessageFormat.format(Lang.BALANCE_HAS_NO_FUNDS, this.economyService.formatBigDecimal(account.getBalance()), this.economyService.formatBigDecimal(value)));
+                return true;
+            }
+
+            account.setBalance(account.getBalance().subtract(value));
+            targetAccount.setBalance(targetAccount.getBalance().add(value));
+
+            account = this.economyService.saveInCache(account);
+            targetAccount = this.economyService.saveInCache(targetAccount);
+
+            sender.sendMessage(MessageFormat.format(Lang.PAYMENT_SEND_SUCCESS, this.economyService.formatCurrency(value), targetAccount.getOwnerName()));
+
+            Player targetPlayer = Bukkit.getPlayer(targetAccount.getOwnerName());
+            if (targetPlayer != null)
+                targetPlayer.sendMessage(MessageFormat.format(Lang.PAYMENT_RECEIVED_SUCCESS, this.economyService.formatCurrency(value), account.getOwnerName()));
+
             return true;
         }
         return false;
