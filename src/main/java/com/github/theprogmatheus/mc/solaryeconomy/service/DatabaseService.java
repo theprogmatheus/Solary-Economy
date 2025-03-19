f1a1ee0d60fbc7c5eed2fd23af5071cc2d169343
theprogmatheus <theprog.matheus@gmail.com>
2025-03-19 20:10:58 -0300

    Add TycoonEntity class on dbManager

--- src/main/java/com/github/theprogmatheus/mc/solaryeconomy/service/DatabaseService.java
+++ src/main/java/com/github/theprogmatheus/mc/solaryeconomy/service/DatabaseService.java
@@ -3,6 +3,7 @@ package com.github.theprogmatheus.mc.solaryeconomy.service;
 import com.github.theprogmatheus.mc.solaryeconomy.database.DatabaseManager;
 import com.github.theprogmatheus.mc.solaryeconomy.database.entity.BankAccountEntity;
 import com.github.theprogmatheus.mc.solaryeconomy.database.entity.BankEntity;
+import com.github.theprogmatheus.mc.solaryeconomy.database.entity.TycoonEntity;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,6 +25,7 @@ public class DatabaseService implements Service {
         // Add database entities
         this.databaseManager.addEntityClass(BankEntity.class);
         this.databaseManager.addEntityClass(BankAccountEntity.class);
+        this.databaseManager.addEntityClass(TycoonEntity.class);
 
         // Start database
         this.databaseManager.startDatabase();
