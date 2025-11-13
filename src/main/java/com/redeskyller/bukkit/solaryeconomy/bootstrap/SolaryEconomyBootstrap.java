c86487890df428bf7035c769d815bffcec3b0f01
theprogmatheus <theprog.matheus@gmail.com>
2025-11-13 20:17:22 -0300

    Remove legacy code.

---
+++ src/main/java/com/redeskyller/bukkit/solaryeconomy/bootstrap/SolaryEconomyBootstrap.java
@@ -0,0 +1,18 @@
+package com.redeskyller.bukkit.solaryeconomy.bootstrap;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SolaryEconomyBootstrap {
+
+    public static void bootstrap(JavaPlugin plugin) {
+
+    }
+
+    public static void enable(JavaPlugin plugin) {
+
+    }
+
+    public static void disable(JavaPlugin plugin) {
+
+    }
+}
