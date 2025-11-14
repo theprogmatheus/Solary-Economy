4a5be945634dc43e219f0e14e7f6d96773463315
theprogmatheus <theprog.matheus@gmail.com>
2025-11-13 22:17:53 -0300

    Create Account and Economy implementations

---
+++ src/main/java/com/redeskyller/bukkit/solaryeconomy/api/impl/AccountImpl.java
@@ -0,0 +1,52 @@
+package com.redeskyller.bukkit.solaryeconomy.api.impl;
+
+import com.redeskyller.bukkit.solaryeconomy.api.Account;
+import com.redeskyller.bukkit.solaryeconomy.api.Economy;
+import com.redeskyller.bukkit.solaryeconomy.util.IntFlag;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+public class AccountImpl implements Account {
+
+    private final Economy economy;
+    private final String nameId;
+    private final UUID ownerId;
+    private final BigDecimal balance;
+    private final IntFlag flags;
+
+    @Override
+    public Economy getEconomy() {
+        return this.economy;
+    }
+
+    @Override
+    public String getNameId() {
+        return this.nameId;
+    }
+
+    @Override
+    public UUID getOwnerId() {
+        return this.ownerId;
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public boolean hasFlag(AccountFlag flag) {
+        return this.flags.hasFlag(flag.getFlagValue());
+    }
+
+    @Override
+    public void setFlag(AccountFlag flag, boolean value) {
+        if (value)
+            this.flags.addFlag(flag.getFlagValue());
+        else
+            this.flags.removeFlag(flag.getFlagValue());
+    }
+}
---
+++ src/main/java/com/redeskyller/bukkit/solaryeconomy/api/impl/EconomyImpl.java
@@ -0,0 +1,73 @@
+package com.redeskyller.bukkit.solaryeconomy.api.impl;
+
+import com.redeskyller.bukkit.solaryeconomy.api.Economy;
+import com.redeskyller.bukkit.solaryeconomy.util.IntFlag;
+import lombok.Data;
+
+@Data
+public class EconomyImpl implements Economy {
+
+    private final String nameId;
+    private final String name;
+    private final String description;
+    private final String command;
+    private final String[] commandAliases;
+    private final String currencySymbol;
+    private final String currencyName;
+    private final String currencyNamePlural;
+    private final IntFlag flags;
+
+
+    @Override
+    public String getNameId() {
+        return this.nameId;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public String getCommand() {
+        return this.command;
+    }
+
+    @Override
+    public String[] getCommandAliases() {
+        return this.commandAliases;
+    }
+
+    @Override
+    public String getCurrencySymbol() {
+        return this.currencySymbol;
+    }
+
+    @Override
+    public String getCurrencyName() {
+        return this.currencyName;
+    }
+
+    @Override
+    public String getCurrencyNamePlural() {
+        return this.currencyNamePlural;
+    }
+
+    @Override
+    public boolean hasFlag(EconomyFlag flag) {
+        return this.flags.hasFlag(flag.getFlagValue());
+    }
+
+    @Override
+    public void setFlag(EconomyFlag flag, boolean value) {
+        if (value)
+            this.flags.addFlag(flag.getFlagValue());
+        else
+            this.flags.removeFlag(flag.getFlagValue());
+    }
+}
